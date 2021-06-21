ad2f5562535ca262c9023e76e334b41c14694a9e
Wilfried PAILLOT <paillotwilfried@gmail.com>
2021-06-21 10:03:25 +0200

    Init page deleteAccount.php

---
+++ deleteAccount.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Load head -->
+    <?php require_once('./component/head.php'); ?>
+</head>
+<body>
+    <!-- Load connected header -->
+    <?php require_once('./component/header/headerConnected.php'); ?>
+
+    <h1>TEST</h1>
+
+    <!-- Load footer-->
+    <?php require_once('./component/footer.php'); ?>
+</body>
+</html>
