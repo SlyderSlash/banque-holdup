9208759502168fc9f78d3d7fe3cd2ae06c532639
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2021-07-06 16:03:47 +0200

    ajout du fichier sql

--- component/security.php
+++ component/security.php
@@ -2,9 +2,6 @@
 session_start();
 function testName ($value){
     $value = htmlspecialchars($value);
-    //Si ton client te met en valeur <h1>BOUH</h1>
-    //Le PHP destruira la balise
-    //Afin d'éviter une injection de code HTML
     if (strlen($value) >= 150 || 
         strlen($value) <= 1 || 
         preg_match('/[0-9]/',$value)){
@@ -12,15 +9,6 @@ function testName ($value){
         }
     else return true;
 }
-// Tu va devoir verifier l'adresse email via une fonction
-// Si elle n'es pas bonne t'as fonction renverra false
-// Dans le cas contraire elle renverra l'adresse email après le passage en htmlspecialschars
-// ATTENTION : Dans le cas des adresses e-mail et de PHP tu n'as pas besoin de REGEX
-// Fait des recherches , Test des choses, prends des risques et on verra si tu a raison ou tort
-// Mais dans tous les cas tu apprendra :D
-// P.S N'oublie pas de tester t'es valeur avec une variable et des var_dump
-// Bon courage et si tu a besoin, ton fidèle serviteur reste dispo :D
-
 
 function testEmail($value){
     if(filter_var($value, FILTER_VALIDATE_EMAIL)){
