12b0c4a8dc9932b0c03eb614f15fa2204bb678b4
Wilfried PAILLOT <paillotwilfried@gmail.com>
2021-07-06 13:17:21 +0200

    Création class Utilisateurs + préparation setters/getters

---
+++ component/script/Utilisateurs.php
@@ -0,0 +1,112 @@
+<?php
+
+class Utilisateurs {
+
+private $erreurs = [],
+        $id,
+        $iban,
+        $banquierId,
+        $genre,
+        $nom,
+        $prenom,
+        $adresse,
+        $codepostal,
+        $ville,
+        $naissance,
+        $mail,
+        $pass,
+        $pi,
+        $suppress;
+
+// Setters
+
+    public function setId($id) {
+
+    }
+    public function setIban($iban) {
+        
+    }
+    public function setBanquierId($banquierId) {
+        
+    }
+    public function setGenre($genre) {
+        
+    }
+    public function setNom($nom) {
+        
+    }
+    public function setPrenom($prenom) {
+        
+    }
+    public function setAdresse($adresse) {
+        
+    }
+    public function setCodepostal($codepostal) {
+        
+    }
+    public function setVille($ville) {
+        
+    }
+    public function setNaissance($naissance) {
+        
+    }
+    public function setMail($mail) {
+        
+    }
+    public function setPass($pass) {
+        
+    }
+    public function setPi($pi) {
+        
+    }
+    public function setSuppress($suppress) {
+        
+    }
+
+
+// Getters
+
+    public function getId($id) {
+
+    }
+    public function getIban($iban) {
+        
+    }
+    public function getBanquierId($banquierId) {
+        
+    }
+    public function getGenre($genre) {
+        
+    }
+    public function getNom($nom) {
+        
+    }
+    public function getPrenom($prenom) {
+        
+    }
+    public function getAdresse($adresse) {
+        
+    }
+    public function getCodepostal($codepostal) {
+        
+    }
+    public function getVille($ville) {
+        
+    }
+    public function getNaissance($naissance) {
+        
+    }
+    public function getMail($mail) {
+        
+    }
+    public function getPass($pass) {
+        
+    }
+    public function getPi($pi) {
+        
+    }
+    public function getSuppress($suppres) {
+        
+    }
+
+}
