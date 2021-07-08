a87c08db66645119324f97835d688e4ff7fe4dea
dems S <dems75018@hotmail.fr>
2021-07-08 16:24:28 +0200

    avancement code UPDATE

--- update.php
+++ update.php
@@ -49,15 +49,67 @@ class Update {
       }
 
     }
-
-
   
 
   function validDelete(){} // On les fera ensembles // soit tu te renseigne sur DeleteData
-  function deleteBenif(){}
-  function askdeletclient(){}
-  function virement(){}
+
+  function deleteBenif($db, $clientid , $token, $benif)
+  {
+      $bankerid = DB::getBankerIDwithToken($bankerid);
+      $token = htmlspecialchars($token);
+      $benif = htmlspecialchars($benif);
+      $clientid = htmlspecialchars($clientid);
+
+      if (!bankerid){
+          return false;
+      }
+      else {
+
+      try
+      {
+        $stmt = $db->prepare("UPDATE beneficiaire SET valid=NOW() WHERE id=:benif AND clientid = :clientid");
+        $stmt ->bind_param(':benif', $benif);
+        $stmt ->bind_param(':idclient', $idclient);
+        $stmt ->execute();
+        return true; 
+
+    }catch(PDOException $e) {
+        return false;
+       }
+     }
+
+  }
+
+
+  function askdeletclient($db, $clientid, $token)
+  {
+      $bankeridbankerid = DB::getBankerIDwithToken($bankerid);
+      $token = htmlspecialchars($token);
+      $clientid = htmlspecialchars($clientid);
+
+      if (!bankerid){
+        return false;
+    }
+    else {
+
+    try
+    {
+      $stmt = $db->prepare("UPDATE client SET valid=NOW() WHERE id=:client");
+      $stmt ->bind_param(':idclient', $idclient);
+      $stmt ->execute();
+      return true;
+
+    }catch(PDOException $e) {
+      return false;
+    
+    
+    }
+  }
+ 
 }
+
+  function virement(){}
+
 
 
 ?>
