ce2a90fa8321bf3c7f75dce81154a43df52c043e
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2021-07-29 16:13:20 +0200

    end client function

--- component/function/FunctionBanquier.php
+++ component/function/FunctionBanquier.php
@@ -8,11 +8,50 @@ class FunctionsBanquier{
         $email = Security::testPass($password);
 
         if($db && $email && $password){
-            if(DB::loginBanker($db,$email,$password)){
+            if(noauthDB::getBankerId($db,$email,$password)){
                 return true;
             }else{
                 return false;
             }
+        }
+    }
+    
+
+    public function validClient($db, $idclient, $idbanker){
+        if($db && $idclient && $idbanker){
+            if(DB::validClient($db, $idclient, $idbanker)){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    public function validBeneficiaire($db,$idclient,$idbanker){
+        if($db && $idclient && $idbanker){
+            if(DB::validBeneficiaire($db,$idclient,$idbanker)){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+  
+
+    public function validDeleteClient($db, $idclient, $idbanker){
+        if($db && $idclient && $idbanker){
+            if(DB::validDeleteClient($db, $idclient, $idbanker)){
+                return true;
+            }else{
+                false;
+            }
+        }else{
+            return true;
         }
     }
 
