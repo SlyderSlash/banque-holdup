f0653e04c564fec3378c86f3999f53db396cd1b8
dems S <dems75018@hotmail.fr>
2021-08-03 16:28:08 +0200

    Co-authored-by: SlyderSlash <SlyderSlash@users.noreply.github.com> Co-authored-by: Sverine <Sverine@users.noreply.github.com>

--- component/function/FunctionBanquier.php
+++ component/function/FunctionBanquier.php
@@ -5,39 +5,80 @@ class FunctionsBanquier{
 
     public function loginBanker($db, $email, $password){
         $email = Security::testEmail($email);
-        $email = Security::testPass($password);
+        $password = Security::testPass($password);
+        $idbanquier = NoAuthDB::getBankerId($db,$email,$password);
 
-        if($db && $email && $password){
-            if(noauthDB::getBankerId($db,$email,$password)){
-                return true;
-            }else{
-                return false;
+        switch(false){
+            case $email:
+                $_SESSION['error']= "L'adresse n'est pas au bon format";
+                header('Location: ../bankeraccess.php'); 
+                break; 
+
+            case $password:
+                $_SESSION['error']= "Le mot de passe n'est pas au bon format";
+                header('Location: ../bankeraccess.php');
+                break;
+
+            default:
+            if (!$idbanquier) {
+                $_SESSION['error']= "Problême d'identifiant";
+                header('Location: ../bankeraccess.php');
+                break;
             }
-        }
+
+            else {
+                $token = BanquierDB::PUTToken($idbanquier);
+                if (!$token){
+                    $_SESSION['error']= "Problème technique";
+                    header('Location: ../bankeraccess.php');
+                }
+                else {
+                    $_SESSION['token'] = $token;
+                    $_SESSION['success']="Bienvenue !";
+                    header('Location: ../dashboardbanquier.php'); // Ici, il manque la page du dashboard banquier
+                }
+                break;
+            }
+        }   
     }
     
 
-    public function validClient($db, $idclient, $idbanker){
-        if($db && $idclient && $idbanker){
-            if(DB::validClient($db, $idclient, $idbanker)){
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return false;
+    public function validClient($idclient, $idbanker){
+        
+        switch(false){
+            case ClientDB::PUTClient($idclient): // Ici on est pas surs des fonctions qu'ils ont a dev. ni leur noms
+                $_SESSION['error']= "Problème technique lors de la validation du client";
+                header('Location: ../bankeraccess.php'); 
+                break; 
+                
+            case BanquierDB::PUTBanquier($idbanquier): // Ici on pas surs non plus, est ce que cette fonction est sencée existée ? ^^
+                $_SESSION['error'] = "Problème technique lors de la validation du client";
+                header('Location: ../bankeraccess.php'); 
+                
+                default:
+                $_SESSION['success']= "La validation du client à bien été prise en compte";
+                header('Location: ../bankeraccess.php'); 
+            break;
         }
     }
 
+    
     public function validBeneficiaire($db,$idclient,$idbanker){
-        if($db && $idclient && $idbanker){
-            if(DB::validBeneficiaire($db,$idclient,$idbanker)){
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return false;
+        
+        switch(false){
+            case ClientDB::PUTClient($idclient): // Ici on est pas surs des fonctions qu'ils ont a dev. ni leur noms
+                $_SESSION['error']= "Problème technique lors de la validation du bénéficiaire";
+                header('Location: ../bankeraccess.php'); 
+                break; 
+
+            case BanquierDB::PUTBanquier($idbanquier): // Ici on pas surs non plus, est ce que cette fonction est sencée existée ? ^^
+                $_SESSION['error'] = "Problème technique lors de la validation du bénéficiaire";
+                header('Location: ../bankeraccess.php'); 
+                    
+                default:
+                $_SESSION['success']= "La validation du bénéficiaire à bien été prise en compte";
+                header('Location: ../bankeraccess.php'); 
+            break;
         }
     }
 
