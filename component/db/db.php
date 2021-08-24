5dbb7b4b20b269ce18d6b27ff75df7d16c8463de
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2021-08-24 18:03:24 +0200

    Update

--- component/db/db.php
+++ component/db/db.php
@@ -1,15 +1,11 @@
 <?php
-// config part
-$dbhost = "15.188.57.143:3306";
-$dbuser = "team";
-$dbpass = "F:g8#Hc94";
-$dbname = "DBHoldUp";
 
 // BDD connexion
 trait DB {
   private $pdo = null;
   public function __construct() {
     try {
+      require_once 'dbconfig.php';
       $this->pdo = new PDO("mysql:host=$dbhost; dbname=$dbname", $dbuser, $dbpass);
       $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $err) {
