2848b0d8acd93b925d7305ddcec89dd68e947fff
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2021-08-24 18:10:01 +0200

    Update

--- component/db/db.php
+++ component/db/db.php
@@ -5,7 +5,7 @@ trait DB {
   private $pdo = null;
   public function __construct() {
     try {
-      require_once './component/db/dbconfig.php';
+      require_once ('./component/db/dbconfig.php');
       $this->pdo = new PDO("mysql:host=$dbhost; dbname=$dbname", $dbuser, $dbpass);
       $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $err) {
