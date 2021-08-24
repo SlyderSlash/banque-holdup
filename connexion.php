e63d3eb8e1494fa0fc136185eddfb801428e6692
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2021-08-24 16:40:04 +0200

    Update

--- connexion.php
+++ connexion.php
@@ -11,6 +11,7 @@ require_once('./component/function/FunctionsClient.php');
 //require_once(); function/signIn
 //require_once(); function/logIn
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    echo '<Script> console.log("c\'est envoyé")</Script>';
     if ($_POST['type'] === 'si'){
         FunctionsClient::signIn(
             $_POST['name'],
