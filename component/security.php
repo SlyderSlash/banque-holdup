044a77802ad9bf4c2b4271e782323104dee33bcc
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2021-08-31 15:55:27 +0200

    Update

--- component/security.php
+++ component/security.php
@@ -29,7 +29,7 @@ class Security{
                 return false; 
             }
             else {
-                $pass="tn6YJ!=-48Xc2z"+$pass+"tnPlu!=-48Xc2z";
+                $pass="tn6YJ8Xc2z".$pass."tnPlu8Xc2z";
                 $pass=password_hash($pass, PASSWORD_DEFAULT);
                 return $pass; // ICI retourner le mot de passe hashé
             }
