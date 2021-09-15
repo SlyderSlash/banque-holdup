f87229d9ca18600793c39d4cc23547966f614179
WilfriedPaillot <paillotwilfried@gmail.com>
2021-09-15 21:44:11 +0200

    Add a reverse button

--- component/script/bankerNavigation.js
+++ component/script/bankerNavigation.js
@@ -5,21 +5,27 @@ for (let i = 0; i < buttons.length; i++) {
     // iteration
     buttons[i].addEventListener('click', function () {
         //Print the name value of the clicked button
-        console.log(buttons[i].name);
-
+        //console.log(buttons[i].name);
         let button = document.getElementsByName(buttons[i].name);
-        console.log(button);
+        //console.log(button);
         if (button[0].name ==='account_creation') {
-          document.getElementById('globalSection').classList.add('d-none');
+          document.getElementById('globalSection').classList.toggle('d-none');
           document.getElementById('account_creation').classList.toggle('d-none');
         }
-        if (button[0].name ==='account_beneficiaries') {
-          document.getElementById('globalSection').classList.add('d-none');
+        else if (button[0].name ==='account_beneficiaries') {
+          document.getElementById('globalSection').classList.toggle('d-none');
           document.getElementById('account_beneficiaries').classList.toggle('d-none');
         }
-        if (button[0].name ==='account_deletion') {
-          document.getElementById('globalSection').classList.add('d-none');
-          document.getElementById('account_deletion').classList.toggle('d-none');
+        else if (button[0].name ==='account_deletion') {
+          document.getElementById('globalSection').classList.toggle('d-none');
+          document.getElementById('account_deletion').classList.replace('d-none','activeSection' );
+        }
+        // Think to add button to back on choices
+        else if (button[0].name ==='backToChoices') {
+        //retrieve display of choices
+        document.getElementById('globalSection').classList.toggle('d-none');
+        //hide active...
+        document.querySelector('.activeSection').classList.replace('activeSection','d-none');
         }
     });
 }
